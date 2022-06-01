66bc2ea5cfe7750434986edd799286112af64753
rybalchenko-eugene <106405926+rybalchenko-eugene@users.noreply.github.com>
2022-06-01 23:48:17 +0300

    adding username

--- Example_001/Program.cs
+++ Example_001/Program.cs
@@ -1,3 +1,15 @@
 ﻿float numA = 14;
 float numB = 5;
 Console.WriteLine (numA / numB);
+
+Console.WriteLine("Введите ваше имя: ");
+string username = Console.ReadLine();
+if (username == "Женя")
+{
+    Console.WriteLine("Ура, наконец-то Женя! ");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
